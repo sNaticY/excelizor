fbec203588bf05d80384eb4ffa1d77c21e9f5d09
sNatic <snatic@outlook.com>
2018-01-14 02:12:39 +0100

    Correct c# template using Add c# template namespace

--- exports/SimpleTypes.cs
+++ exports/SimpleTypes.cs
@@ -1,23 +1,27 @@
-using System;
+using System.Collections.Generic;
 
-public class SimpleTypes {
-    public int Id;
-    public int NumberTest;
-    public string StringTest;
-    public float FloatTest;
-    public bool BoolTest;
-    public Dictionary<string, float> DictTest1;
-    public Dictionary<string, float> DictTest2;
-    public List<string> ListTest1;
-    public List<string> ListTest2;
-    public Dictionary<string, Dictionary<string, string>> DictTest3;
-    public Dictionary<string, Dictionary<string, string>> DictTest4;
-    public Dictionary<string, Dictionary<string, string>> DictTest5;
-    public List<List<string>> ListTest3;
-    public List<List<string>> ListTest4;
-    public List<List<string>> ListTest5;
-    public Dictionary<string, List<string>> DictTest6;
-    public Dictionary<string, List<string>> DictTest7;
-    public List<Dictionary<string, string>> ListTest6;
-    public List<Dictionary<string, string>> ListTest7;
+namespace Configs
+{
+    public class SimpleTypes 
+    {
+        public int Id;
+        public int NumberTest;
+        public string StringTest;
+        public float FloatTest;
+        public bool BoolTest;
+        public Dictionary<string, float> DictTest1;
+        public Dictionary<string, float> DictTest2;
+        public List<string> ListTest1;
+        public List<string> ListTest2;
+        public Dictionary<string, Dictionary<string, string>> DictTest3;
+        public Dictionary<string, Dictionary<string, string>> DictTest4;
+        public Dictionary<string, Dictionary<string, string>> DictTest5;
+        public List<List<string>> ListTest3;
+        public List<List<string>> ListTest4;
+        public List<List<string>> ListTest5;
+        public Dictionary<string, List<string>> DictTest6;
+        public Dictionary<string, List<string>> DictTest7;
+        public List<Dictionary<string, string>> ListTest6;
+        public List<Dictionary<string, string>> ListTest7;
+    }
 }
